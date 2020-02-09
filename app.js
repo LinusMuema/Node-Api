80b4afcf2bff943105e9ab34445f5d6edff4b892
linus muema <linus.m.muema@gmail.com>
2020-02-09 15:13:21 +0300

    Added the dependancies

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+require("dotenv").config()
+
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on("error", (err)=>{console.error(err)})
+db.once("open", () => {console.log("DB started successfully")})
+
+app.listen(2400, () => {console.log("Server started: 2400")})
