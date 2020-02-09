008eeea0f1d05acc674a0ed79bc427a9ebbe5163
linus muema <linus.m.muema@gmail.com>
2020-02-09 15:26:55 +0300

    Added schema

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,23 @@
 const express =  require("express")
 const router = express.Router()
+const User = require("../models/user")
 
 //Get All users
 router.get("/", (req, res)=>{
+    User.find()
+    .then((response) => {res.json(response)})
+    .catch((err) => {res.json({message: err.message})})
 })
 
 //Create new user
 router.post("/", (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        age: req.body.age
+    })
+    user.save()
+    .then((response) => {res.json(response)})
+    .catch((err) => {res.json({message: err.message})})
 })
 
 module.exports = router
