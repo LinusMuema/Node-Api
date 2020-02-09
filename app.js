415768aa3668dfd6180831f9b2a314a231d6d560
linus muema <linus.m.muema@gmail.com>
2020-02-09 15:19:47 +0300

    Added the endpoints

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ require("dotenv").config()
 
 const express = require("express")
 const mongoose = require("mongoose")
+const router = express.Router()
+const route = require("./routes/users")
 const app = express()
+
+app.use(express.json())
+app.use("/users", route)
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
+
 db.on("error", (err)=>{console.error(err)})
 db.once("open", () => {console.log("DB started successfully")})
 
 app.listen(2400, () => {console.log("Server started: 2400")})
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express =  require("express")
+const router = express.Router()
+
+//Get All users
+router.get("/", (req, res)=>{
+})
+
+//Create new user
+router.post("/", (req, res) => {
+})
+
+module.exports = router
